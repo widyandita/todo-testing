695e51041d1183464da7399049361c497ebcfa14
widyandita <idaayuwidyanandita@gmail.com>
2026-08-06 16:01:42 +0800

    ran test ID WT-002

--- tests/todoapp-test.spec.ts
+++ tests/todoapp-test.spec.ts
@@ -4,4 +4,42 @@ test('page loaded and has title', async ({ page }) => {
     await page.goto('https://todomvc.com/examples/react/dist/#/');
 
     await expect(page).toHaveTitle('TodoMVC: React');
-})
+});
+
+test('add, check and delete to-do list', async ({ page }) => {
+    await page.goto('https://todomvc.com/examples/react/dist/#/');
+
+    const newTodo = page.getByTestId('text-input');
+
+    // Add tasks
+    await newTodo.click();
+    await newTodo.fill('sort videos');
+    await newTodo.press('Enter');
+    await newTodo.fill('edit pictures');
+    await newTodo.press('Enter');
+    await newTodo.fill('post pictures');
+    await newTodo.press('Enter');
+    await newTodo.fill('design packaging');
+    await newTodo.press('Enter');
+
+    // Verify the amount of tasks are 4
+    const tasks = page.locator('.todo-list li');
+
+    await expect(tasks).toHaveCount(4);
+
+    // Check "sort videos"
+    await page.getByRole('listitem').filter({ hasText: 'sort videos' }).getByTestId('todo-item-toggle').check();
+    
+    // Verify the completed task is 1
+    await expect(page.locator('.todo-list li.completed')).toHaveCount(1);
+    
+    // Delete "post photos"
+    await tasks.nth(2).hover();
+    await tasks.nth(2).locator('.destroy').click({force: true});
+
+    // Clear tasks
+    await page.locator('.clear-completed').click({force: true});
+
+    // Verify the remaining tasks are 2
+    await expect(tasks).toHaveCount(2)
+});
